880f894a06387ca3d04da256f4a970192debce8c
Данил Чебанский <dan14n97@yandex.ru>
2024-07-06 14:34:22 +0300

    Added files and migrations

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.TextField(max_length=600, verbose_name='Описание')
+
+    def __str__(self):
+        # Строковое отображение объекта
+        return self.name
+
+    class Meta:
+        verbose_name = 'категория'  # Настройка для наименования одного объекта
+        verbose_name_plural = 'категории'  # Настройка для наименования набора объектов
+        ordering = ('name',)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.TextField(max_length=600, verbose_name='Описание')
+    image = models.ImageField(upload_to='products/', verbose_name='Изображение(превью)', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    price = models.IntegerField(verbose_name='Цена за покупку')
+    created_at = models.DateField(verbose_name='Дата создания')
+    updated_at = models.DateField(verbose_name='Дата последнего изменения')
+    manufactured_at = models.DateField(verbose_name='Дата производства продукта')
+
+    def __str__(self):
+        # Строковое отображение объекта
+        return f'{self.name} {self.price}'
+
+    class Meta:
+        verbose_name = 'продукт'  # Настройка для наименования одного объекта
+        verbose_name_plural = 'продукты'  # Настройка для наименования набора объектов
+        ordering = ('name',)
