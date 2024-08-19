d2966341b058edbd052cdf1cf5fc5c583d5ec0c1
Danil Chebansku <dan14n97@yandex.ru>
2024-08-19 17:08:57 +0300

    DZ 22.1(repaired)

--- catalog/forms.py
+++ catalog/forms.py
@@ -4,7 +4,17 @@ from django.forms import ModelForm, BooleanField
 from catalog.models import Product, Version
 
 
-class ProductForm(ModelForm):
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                field.widget.attrs['class'] = 'form-control'
+
+
+class ProductForm(ModelForm, StyleFormMixin):
     class Meta:
         model = Product
         fields = "__all__"
@@ -24,15 +34,7 @@ class ProductForm(ModelForm):
         return cleaned_data
 
 
-class VersionForm(ModelForm):
+class VersionForm(ModelForm, StyleFormMixin):
     class Meta:
         model = Version
         fields = '__all__'
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
-            if isinstance(field, BooleanField):
-                field.widget.attrs['class'] = 'form-check-input'
-            else:
-                field.widget.attrs['class'] = 'form-control'
